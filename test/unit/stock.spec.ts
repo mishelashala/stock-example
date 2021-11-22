5db97457bc5124d58994da764d75c01b335e6562
Michell Ayala <starships@outlook.com>
2021-11-22 09:17:30 -0600

    Update stock.spec.ts

--- test/unit/stock.spec.ts
+++ test/unit/stock.spec.ts
@@ -3,7 +3,7 @@ import { Stock } from "../../src/stock";
 
 describe("Feature: Stock", () => {
   describe("given a stock", () => {
-    describe("when they to calculate its price on 2021-10-10", () => {
+    describe("when they try to calculate its price on 2021-10-10", () => {
       it("should return 10", () => {
         const stock = new Stock(["2021-10-10", 10], [["2021-10-10", 10]]);
         const price = stock.Price("2021-10-10");
